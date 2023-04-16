6aa24ac95ca21db86575eff9279d2ef31d1f06d1
Mauricio Chavez <mauriciochavez792@gmail.com>
2023-04-15 23:28:57 -0400

    added components to donation pagee

--- pages/donation.js
+++ pages/donation.js
@@ -6,9 +6,15 @@ import spanishLocale from "public/locales/spanish/donation";
 import {useRouter} from "next/router";
 import { facebook } from "public/data/links"
 import aboutSvg from "public/images/praying.jpg"
-import About from "@components/Homepage/About";
+import methodsSvg from "public/images/donation/statue-hands.jpg"
 import Hero2 from "@components/Hero/Hero2";
-import hands from "public/images/hands.png"
+import hands from "public/images/donation/makeChange.jpg"
+import Horizontal from "@components/List/Horizaontal";
+import jesusChavez from "public/images/jesusChavez.jpg"
+import image2 from "public/images/mauricio.jpeg"
+import arianeMacias from "public/images/arianeMacias.jpg"
+import Vertical from "@components/List/Vertical";
+import Methods from "@components/Donation/Methods";
 
 export default function Donation(){
 
@@ -22,8 +28,10 @@ export default function Donation(){
         <Fragment>
             <Navbar/>
             <Hero2 language={language} heroButton={facebook} heroBackground={"h-screen bg-[url('..//public/images/coffee.jpg')] bg-cover text-neutral-200 "}/>
-            <Sermons language={language} sermonButton={facebook} svg={aboutSvg}/>
-            <About language={language} aboutButton={"https://cash.app/$EncuentroChurch"} svg={hands}/>
+            <Sermons language={language} sermonButton={facebook} svg={hands}/>
+            <Methods language={language} methodsButton={facebook} svg={methodsSvg}/>
+            <Horizontal language={language} svg1={jesusChavez} svg2={image2} svg3={arianeMacias} id={"opportunities"}/>
+            <Vertical language={language} verticalButton={facebook} svg={aboutSvg}/>
         </Fragment>
     )
 }
